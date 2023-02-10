37528e9415dcf671ec459bb6b8b565577a6a92b2
Mitya00 <kurbanaliev.mintemir@gmail.com>
2023-02-10 12:52:04 +0600

    last

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,17 +1,17 @@
 output public_subnets {
   value       = aws_subnet.public_subnets.*.id
-  sensitive   = true
+  
   description = "outputs of public subnets id "
 }
  
 output "vpc_id" {
-  type = string
+
   value       = aws_vpc.main.id
   description = "outputs of vpc id "
 }
 
 output private_subnets {
-  type =string
+ 
   value       = aws_subnet.private_subnets.*.id
   description = "outputs of public subnets id "
 }
