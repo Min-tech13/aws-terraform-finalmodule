e1cdf6af0a3e82498fd402882bd710688a82e37b
Mitya00 <kurbanaliev.mintemir@gmail.com>
2023-02-10 12:37:34 +0600

    last

--- eks/variables.tf
+++ eks/variables.tf
@@ -11,7 +11,7 @@ variable "instance_type" {
   description = "ID of the specific VPC to retrieve."
 }
 
-variable vpc_id {
+variable "vpc_id" {
   type        = string
   default     = ""
   description = "ID of the specific VPC to retrieve."
