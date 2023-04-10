2c12735c2fdb2d43411b305a2891fc53f4f2691d
Mintemir <123369194+Mitya00@users.noreply.github.com>
2023-04-10 19:47:54 +0600

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -13,11 +13,12 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  enable_dns_hostnames = true
   tags = {
     Name = "${var.env}-vpc"
     environment = "${var.env}"
   }
+  enable_dns_hostnames = true
+  enable_dns_support = true
 }
 
 resource "aws_internet_gateway" "main" {
